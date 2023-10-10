56bf8f75eb999001cc76f9b271da4cf6d1dbe0bb
Alex Breshears <apbresh@gmail.com>
2023-10-10 09:15:53 -0500

    Improved performance

--- gradio-examples/federation/app.py
+++ gradio-examples/federation/app.py
@@ -3,7 +3,6 @@ from datetime import date
 from dateutil.relativedelta import relativedelta
 import pandas as pd
 
-
 pd.options.plotting.backend = "plotly"
 
 from pystarburst import Session
@@ -28,81 +27,69 @@ session_properties = {
     "auth": trino.auth.BasicAuthentication(username, password)
 }
 
-session = Session.builder.configs(session_properties).create()
 
-import time
+class Data():
 
-def to_pandas_df(pystarburst_df):
-    print("starting collect dataframe")
-    start = time.time()
-    
-    sb_df = pystarburst_df.collect()
-    
-    end = time.time()
-    print(f'Operation took {end-start}')
+    def __init__(self):
+        # Setup a session, query history logger, and initial data frames
+        session = Session.builder.configs(session_properties).create()
 
-    print("starting convert to pandas")
-    start = time.time()
-    
-    pd_df = pd.DataFrame(sb_df)
-    
-    end = time.time()
-    print(f'Operation took {end-start}')
+        self.df_onprem_credit = session.table('sep_dataproducts.customer_360.customer_with_credit')
+        self.df_dl_customer_360 = session.table('s3lakehouse.data_product_customer_360.customer_information')
 
-    return pd_df
+        self.df_onprem_credit = self.df_onprem_credit.with_column('risk_appetite', f.sql_expr("replace(\"risk_appetite\", 'wild_west', 'very_low')"))
+        self.df_joined = self.df_onprem_credit.join(self.df_dl_customer_360, self.df_onprem_credit['custkey'] == self.df_dl_customer_360['custkey'])
 
-def get_unique_segs():
-    print("starting build segs dataframe")
-    start = time.time()
-    
-    df_sb = session.table('sep_dataproducts.customer_360.customer_with_credit')
-    segments = df_sb.select('customer_segment').distinct()
-    
-    end = time.time()
-    print(f'Operation took {end-start}')
-    
-    return to_pandas_df(segments)['customer_segment'].to_list()
+        self.segments = self.df_onprem_credit.select('customer_segment').distinct().to_pandas()['customer_segment'].to_list()
 
-def get_agg_data(segment='ALL'):
-    print("starting build agg dataframe")
-    start = time.time()
-    
-    df_dl_customer_360 = session.table("s3lakehouse.data_product_customer_360.customer_information")
-    df_onprem_credit = session.table("sep_dataproducts.customer_360.customer_with_credit")
+        self.query_history = session.query_history()
+        self.queries_list = list()
 
-    df_onprem_credit = df_onprem_credit.with_column("risk_appetite", f.sql_expr("replace(\"risk_appetite\", 'wild_west', 'very_low')"))
-    df_joined = df_onprem_credit.join(df_dl_customer_360, df_onprem_credit['custkey'] == df_dl_customer_360['custkey'])
+    def get_queries(self) -> list:
+        return self.query_history.queries
 
-    if segment != 'ALL':
-        df_joined = df_joined.filter(col('customer_segment') == segment)
+    def get_unique_segs(self) -> pd.DataFrame:
+        return self.segments
 
-    df_summary = df_joined\
-    .group_by("state")\
-    .count()\
-    .sort(col("count"), ascending=False)
-    
-    end = time.time()
-    print(f'Operation took {end-start}')
+    def get_agg_data(self, segment) -> pd.DataFrame:
+        # Drive the aggergate data
 
-    return to_pandas_df(df_summary)
+        if segment != '':
+            df_summary = self.df_joined.filter(col('customer_segment') == segment)\
+            .group_by('state')\
+            .count()\
+            .sort(col('count'), ascending=False)
+        else:
+            df_summary = self.df_joined\
+            .group_by('state')\
+            .count()\
+            .sort(col('count').desc())
+
+        return df_summary.to_pandas()
 
 with gr.Blocks() as demo:
     gr.Markdown(
     """
     # A simple demo showing a data app driven by PyStarburst.
     """)
-    import time
     
-    segs = get_unique_segs()
+    my_data = Data()
+
+    segs = my_data.get_unique_segs()
 
     with gr.Row():
-        seg = gr.Dropdown(segs, label="Segment", value="ALL", allow_custom_value=True)
+        seg = gr.Dropdown(segs, label='Segment', value='silver', allow_custom_value=False)
  
-    #data = get_agg_data()
+    plt = gr.BarPlot(x='state', y='count', tooltip=['state','count'], y_title='Num of Customers', x_title='State')
+    
+    queries = gr.Dataframe([], type='array', interactive=False, wrap=True,
+                           headers=['QueryID', 'QueryText'], datatype=['str', 'str'], label='Query History')
 
-    plt = gr.BarPlot(x='state', y='count', tooltip=['state','count'], y_title="Num of Customers", x_title="State")
+    btn = gr.Button('Refresh Query History')
+    btn.click(my_data.get_queries, [], queries, queue=False)
+    
+    plt.change(my_data.get_queries, [], queries, queue=False)
+    seg.change(my_data.get_agg_data, [seg], plt, queue=False)
+    demo.load(my_data.get_agg_data, [seg], plt, queue=False)
 
-    seg.change(get_agg_data, [seg], plt, queue=False)
-    demo.load(get_agg_data, [seg], plt, queue=False)    
-
-demo.launch(server_name="0.0.0.0")
+demo.queue().launch(server_name="0.0.0.0")
