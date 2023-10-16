6876267fb6469ac0d2e6872dbf3e9c7abc389f6d
Alex Breshears <apbresh@gmail.com>
2023-10-16 18:12:29 -0500

    Bug fixes & hf spaces compat

--- apps/gradio/customer_360_ml/env.py
+++ apps/gradio/customer_360_ml/env.py
@@ -1,21 +1,24 @@
 # Prompt user for credentials
+import os
+
 PROPMPT_CREDS=False
+SHOW_SETTINGS=False
 
 # Web App
-PORT = 8080
+PORT = 7860
 BIND_HOST = '0.0.0.0'
-SHARE = True
+SHARE = False
 DEBUG = False
 
 # Credentials
-HOST=''
-USERNAME=''
-PASSWORD=''
+HOST=os.environ.get("HOST")
+USERNAME=os.environ.get("SB_USER")
+PASSWORD=os.environ.get("SB_PASS")
 
 # Target Catalog for writing
 TARGET_CATALOG='s3lakehouse'
 
 # OpenAI Configs
 OPENAI_MODEL = "gpt-3.5-turbo-16k"
-OPENAI_API_KEY = ""
+OPENAI_API_KEY = os.environ.get("OPEN_API_KEY")
 
